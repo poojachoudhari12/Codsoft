ceab54a11283edaefea799446e8ca9795abb3bb6
Pooja Laxmanram choudhari <poojachoudhari775@gmail.com>
2024-12-14 10:37:51 +0530

    Update QuizGame.java

--- QuizGame.java
+++ QuizGame.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class QuizGame {
-    // Class to represent a quiz question
+   
     static class Question {
         String questionText;
         String[] options;
-        int correctOption; // Index of the correct option (0-based)
+        int correctOption; 
 
         public Question(String questionText, String[] options, int correctOption) {
             this.questionText = questionText;
@@ -22,23 +22,23 @@ public class QuizGame {
             new Question("What is 2 + 2?", new String[]{"3", "4", "5", "6"}, 1)
         };
 
-        int score = 0; // Track the user's score
+        int score = 0; 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Quiz Game!");
         System.out.println("You have 10 seconds to answer each question. Let's begin!\n");
 
-        // Loop through each question
+       
         for (int i = 0; i < questions.length; i++) {
             Question q = questions[i];
             System.out.println("Question " + (i + 1) + ": " + q.questionText);
             
-            // Display options
+            
             for (int j = 0; j < q.options.length; j++) {
                 System.out.println((j + 1) + ". " + q.options[j]);
             }
 
-            // Start timer and allow user input
+           
             long startTime = System.currentTimeMillis();
             long endTime = startTime + 10000; // 10 seconds
             boolean answered = false;
@@ -65,7 +65,7 @@ public class QuizGame {
             Thread.sleep(1000); // Pause before the next question
         }
 
-        // Display final result
+        
         System.out.println("Quiz Over!");
         System.out.println("Your final score: " + score + "/" + questions.length);
 
